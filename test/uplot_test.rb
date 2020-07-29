0b72cef7b93e80a2a16cf14718e46d8c0a38bd42
kojix2 <2xijok@gmail.com>
2020-07-29 11:54:04 +0900

    Prepare a test skeleton

--- test/uplot_test.rb
+++ test/uplot_test.rb
@@ -1,11 +1,7 @@
 require 'test_helper'
 
-class UplotTest < Minitest::Test
+class UplotTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
-    refute_nil ::Uplot::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+    assert_kind_of String, ::Uplot::VERSION
   end
 end
