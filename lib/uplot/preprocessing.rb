59d200cae0730fcdb9fba8de2894decb1c74b22d
kojix2 <2xijok@gmail.com>
2020-08-19 23:47:28 +0900

    Improved error messages

--- lib/uplot/preprocessing.rb
+++ lib/uplot/preprocessing.rb
@@ -8,7 +8,26 @@ module Uplot
       arr = parse_as_csv(input, delimiter)
       headers = get_headers(arr, headers, transpose)
       series = get_series(arr, headers, transpose)
-      Data.new(headers, series)
+      if headers.nil?
+        Data.new(headers, series)
+      else
+        if headers.include?(nil)
+          warn "\e[35mHeaders contains nil in it.\e[0m"
+        elsif headers.include? ""
+          warn "\e[35mHeaders contains \"\" in it.\e[0m"
+        end
+        h_size = headers.size
+        s_size = series.size
+        if h_size == s_size
+          Data.new(headers, series)
+        elsif h_size > s_size
+          warn "\e[35mThe number of headers is greater than the number of series.\e[0m"
+          exit 1
+        elsif h_size < s_size
+          warn "\e[35mThe number of headers is less than the number of series.\e[0m"
+          exit 1
+        end
+      end
     end
 
     def parse_as_csv(input, delimiter)
