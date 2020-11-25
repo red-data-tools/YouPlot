9849898cb199cf4a71e16255320553be12105258
kojix2 <2xijok@gmail.com>
2020-11-25 17:21:01 +0900

    Change the test directory from uplot to youplot

--- test/youplot/backends/unicode_plot_backend_test.rb
+++ test/youplot/backends/unicode_plot_backend_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../test_helper'
+require_relative '../../test_helper'
 
 class YouPlotPlotTest < Test::Unit::TestCase
 end
--- test/youplot_test.rb
+++ test/youplot_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require_relative 'test_helper'
 
 class YouPlotTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
